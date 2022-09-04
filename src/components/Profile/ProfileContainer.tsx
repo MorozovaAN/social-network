aac76c84b21545d2763791011789d7b93c6d0cb1
Morozova Anastasiya <MorozovaAnastasiyaN@yandex.ru>
2022-09-04 13:31:26 +0300

    feat: lessons 60, add parameters from react router to navigate to user's personal profile

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Profile } from "./Profile";
 import axios from "axios";
 import { connect } from "react-redux";
 import { AppStateType } from "../../redux/redux-store";
 import { setUserProfile } from "../../redux/profile-reducer";
+import { useParams } from "react-router-dom";
 
-class ProfileContainer extends React.Component<any, any> {
-  componentDidMount() {
+// class ProfileContainer extends React.Component<any, any> {
+//   componentDidMount() {
+//     const params = useParams<"userId">();
+//     const p = params;
+//     console.log(p.userId);
+//     axios
+//         .get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+//         .then((response) => {
+//           this.props.setUserProfile(response.data);
+//         });
+//   }
+//   render() {
+//     return <Profile {...this.props} profile={this.props.profile} />;
+//   }
+// }
+
+const ProfileContainer = (props: any) => {
+  const params = useParams<"*">();
+  let profileId = params["*"];
+
+  if (!profileId) {
+    profileId = "2";
+  }
+  useEffect(() => {
     axios
-      .get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+      .get(`https://social-network.samuraijs.com/api/1.0/profile/${profileId}`)
       .then((response) => {
-        this.props.setUserProfile(response.data);
+        props.setUserProfile(response.data);
       });
-  }
-  render() {
-    return <Profile {...this.props} profile={this.props.profile} />;
-  }
-}
+  }, []);
 
+  return <Profile {...props} profile={props.profile} />;
+};
 type MapStateToPropsType = {
   profile: any;
 };
