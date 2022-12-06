8b29f8444f04691b673fbd070b313995127d3d7b
Morozova Anastasiya <MorozovaAnastasiyaN@yandex.ru>
2022-12-06 17:27:07 +0400

    feat: lessons 75, add redux-form from login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,51 @@
-import React from "react";
+import React, { FC } from "react";
+import s from "./Login.module.css";
+import { Field, InjectedFormProps, reduxForm } from "redux-form";
 
 export const Login = () => {
-  return <h1>login</h1>;
+  const onSubmit = (formData: FormDataType) => {
+    console.log(formData);
+  };
+
+  return (
+    <>
+      <h1>login</h1>
+      <LoginReduxForm onSubmit={onSubmit} />
+    </>
+  );
 };
+type FormDataType = {
+  login: string;
+  password: string;
+  rememberMe: boolean;
+};
+export const LoginForm: FC<InjectedFormProps<FormDataType>> = (props) => {
+  const { handleSubmit } = props;
+
+  return (
+    <form onSubmit={handleSubmit} className={s.form}>
+      <Field
+        component={"input"}
+        name={"login"}
+        type="text"
+        placeholder="login"
+      />
+      <Field
+        component={"input"}
+        name={"password"}
+        type="password"
+        placeholder="password"
+      />
+      <div>
+        <Field component={"input"} name={"rememberMe"} type="checkbox" />
+        <span>remember me</span>
+      </div>
+
+      <button className={s.btnSubmit}>log in</button>
+    </form>
+  );
+};
+
+const LoginReduxForm = reduxForm<FormDataType>({
+  form: "login",
+})(LoginForm);
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -4,12 +4,14 @@ import dialoguesReducer from "./reducers/dialodues-reducer";
 import usersReducer from "./reducers/users-reducer";
 import { authReducer } from "./reducers/auth-reducer";
 import thunkMiddleware from "redux-thunk";
+import { reducer as formReducer } from "redux-form";
 
 let rootReducer = combineReducers({
   profilePage: profileReducer,
   dialoguesPage: dialoguesReducer,
   usersPage: usersReducer,
   auth: authReducer,
+  form: formReducer,
 });
 export type AppStateType = ReturnType<typeof rootReducer>;
 export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
