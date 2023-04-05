0db0417ad418a46eb00774c72bde0678ced534d3
Morozova Anastasiya <MorozovaAnastasiyaN@yandex.ru>
2023-04-05 14:12:30 +0400

    feat: add HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
